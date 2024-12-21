316734438490a84c1fe923327726ba90ab6c4151
AYESHA ASGHAR <ayeshh7766@gmail.com>
2024-12-21 06:51:45 -0800

    Add files via upload

---
+++ javascrip assignment 02/ch03.js
@@ -0,0 +1,46 @@
+// CHAPTER NO : 03
+// VARIABLES FOR NUMBERS
+
+// QUESTION NO 01
+// Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// ANSWER NO 01
+// var age = "i am 18 years old"
+// alert(age)
+
+
+// QUESTION NO 02
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// ANSWER NO 02
+// var numVisits = localStorage.getItem('numVisits');
+
+
+
+
+// QUESTION NO 03
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// ANSWER NO 03
+//  var birthYear = "my birthYear is 2003"
+//  document.write(birthYear)
+
+// QUESTION NO 04
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ 
+// ANSWER NO 04
+//  var  Visitors_name = "john_Doe"
+//  var Product_title = "ordered 5"
+//  var ordered = " T-shirt(s) on XYZ"
+//  document.write(Visitors_name +  Product_title + ordered )
+
+
